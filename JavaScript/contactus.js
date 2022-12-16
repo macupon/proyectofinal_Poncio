b35a931b6e90593ffb2d726a01979a0b21d84d57
macupon <macupon@gmail.com>
2022-12-16 12:52:46 +0100

    cambios en form

--- JavaScript/contactus.js
+++ JavaScript/contactus.js
@@ -11,14 +11,29 @@ let idmess =  document.getElementById("message");
 
 
 // Creamos el evento "click" del boton, el cual tiene "envarForm" como id
-enviarForm.addEventListener("click", () => {
-    // Creamos una Array vacia, que contendra todos los campos no completados por el usuario
-    let arrayForm = [];
-    idname.value === "" && arrayForm.push(idname.placeholder);
-    idemail.value === "" && arrayForm.push(idemail.placeholder);
-    idmess.value === "" && arrayForm.push(idmess.placeholder);
+let escenario = 1
+enviarForm.addEventListener("click", (inp) => {
+    if (escenario == 1) {
+        // Creamos una Array vacia, que contendra todos los campos no completados por el usuario
+        let arrayForm = [];
+        idname.value === "" && arrayForm.push(idname.placeholder);
+        idemail.value === "" && arrayForm.push(idemail.placeholder);
+        idmess.value === "" && arrayForm.push(idmess.placeholder);
 
-    arrayForm.length != 0 ? Swal.fire('Tiene que completar los siguientes campos: ' + arrayForm) : (contactmilo.innerText = "Formulario Enviado!", enviarForm.style = "background-color: #778beb", 
-                                                                        setTimeout(()=> {enviarForm.style = "background-color: #c44569"}, 2000))
-    
+        arrayForm.length != 0 ? Swal.fire('Tiene que completar los siguientes campos: ' + arrayForm) :
+                                        (contactmilo.innerText = "Formulario Enviado!", // enviarForm.style = "background-color: #778beb", 
+                                        setTimeout(()=> {enviarForm.style = "background-color: #596275"}, 1000),
+                                        enviarForm.value = "Send new message",
+                                        escenario = 2)
+        }
+    else if (escenario == 2){
+        enviarForm.style = "background-color: #c44569"
+        contactmilo.innerText = "CONTACT MILO"
+        enviarForm.value = "Send"
+        idname.value = "";
+        idemail.value= "";
+        idmess.value = "";
+        escenario = 1;
+        }
+    inp.preventDefault ();
 })
